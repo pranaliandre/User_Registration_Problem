8fd8040aa84096101933044b34801fef886fd8a4
pranaliandre <pranaliandre06@gmail.com>
2020-03-28 14:23:25 -0500

    Added: Validation of password all rules check

--- userRegistration.sh
+++ userRegistration.sh
@@ -8,8 +8,6 @@ namePattern="([A-Z]{1}[a-z]{2}$)"
 emailPattern="abc((\.[A-Z]+[a-z]*[0-9]*)|(\.[A-Z]*[a-z]+[0-9]*)|(\.[A-Z]*[a-z]*[0-9]+)|^)?@bl\.co(\.[a-z]+){1,}"
 #regex pattern for mobile number
 numberPattern="^([0-9]{2}[ ]{1}[0-9]{10})$"
-#regex pattern for minimum 8 charachter password and one upper case and at least one numeric
-passwordPattern="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)[a-zA-Z0-9]*{7,40}$"
 
 #it take input from user first name
 read -p "Enter the first name: " firstName
@@ -19,8 +17,8 @@ read -p "Enter the last name: " lastName
 read -p "Enter the email address: " emailAddress 
 #it take input from user mobile number
 read -p "Enter the mobile number: " numberM
-#it take input from user (At least 8 minimum charachter) password
-read -p "Enter the password: " passwordChar
+#it take input from user password(8 minimum charachter at least one uppercase character and one special charachter)
+read -p "Enter the password: " password
 
 #function of validation for user first name
 function validationFirstName(){
@@ -57,14 +55,14 @@ validationEmailAddress $emailAddress $emailPattern
 
 #function of validation for password 8 minimum charachter and ate least one uppercase and one numeric value 
 function validationPassword(){
-	if [[ $passwordChar =~ $passwordPattern ]]
+	if [[ ${#password} -ge 8 && "$password" == *[[:lower:]]* && "$password" == *[[:upper:]]* && "$password" == *[0-9]*  && "$password" == *['!'@#\$%^\&*()_+]* ]]
 	then
 		echo "valid password"
 	else
 		echo "Invalid password"
 	fi
 }
-validationPassword $passwordChar $passwordPattern
+validationPassword $password
 
 #function of validation for mobile number
 function validationMobileNumber(){

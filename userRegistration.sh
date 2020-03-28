b4c866df81c68ec1d163f214eeefb746bb4a68c4
pranaliandre <pranaliandre06@gmail.com>
2020-03-28 06:55:38 -0500

    Added: Validation of email address

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,12 +2,16 @@
 shopt -s extglob
 
 echo "Welcome to User Registration"
-#Regex patteren for first name 
+#Regex pattern for first name 
 namePattern="([A-Z]{1}[a-z]{2}$)"
+#regex pattern for email address
+emailPattern="abc((\.[A-Z]+[a-z]*[0-9]*)|(\.[A-Z]*[a-z]+[0-9]*)|(\.[A-Z]*[a-z]*[0-9]+)|^)?@bl\.co(\.[a-z]+){1,}"
 #it take input from user first name
 read -p "Enter the first name: " firstName
 #it take input from userlast name
-read -p "Enter the last name: " lastName 
+read -p "Enter the last name: " lastName
+#it take input from user email address 
+read -p "Enter the email address: " emailAddress 
 #function of validation for user Details
 function validationUserDetails(){
 	local userName=$1
@@ -21,3 +25,4 @@ function validationUserDetails(){
 } 
 validationUserDetails $firstName $namePattern
 validationUserDetails $lastname $namePattern
+validationUserDetails $emailAddress $emailPattern

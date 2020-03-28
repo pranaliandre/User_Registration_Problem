7debf9d1f3d62d2d8695b2eb996387ba20529b4c
pranaliandre <pranaliandre06@gmail.com>
2020-03-28 08:47:40 -0500

    Added: validation for password rule 1 minimum 8 characters

--- userRegistration.sh
+++ userRegistration.sh
@@ -8,6 +8,9 @@ namePattern="([A-Z]{1}[a-z]{2}$)"
 emailPattern="abc((\.[A-Z]+[a-z]*[0-9]*)|(\.[A-Z]*[a-z]+[0-9]*)|(\.[A-Z]*[a-z]*[0-9]+)|^)?@bl\.co(\.[a-z]+){1,}"
 #regex pattern for mobile number
 numberPattern="^([0-9]{2}[ ]{1}[0-9]{10})$"
+#regex pattern forminimum 8 charachter password
+passwordPattern="[a-z]{8,}" 
+
 #it take input from user first name
 read -p "Enter the first name: " firstName
 #it take input from userlast name
@@ -16,27 +19,62 @@ read -p "Enter the last name: " lastName
 read -p "Enter the email address: " emailAddress 
 #it take input from user mobile number
 read -p "Enter the mobile number: " numberM
-#function of validation for user Details
-function validationUserDetails(){
-	local userName=$1
-	local regexPattern=$2
-	if [[ $userName =~ $regexPattern ]]
+#it take input from user (At least 8 minimum charachter) password
+read -p "Enter the password: " passwordChar
+
+#function of validation for user first name
+function validationFirstName(){
+	if [[ $firstName =~ $namePattern ]]
 	then
-		echo "valid details"
+		echo "valid name"
 	else
-		echo "Not valid"
+		echo "invalid first name"
 	fi
 } 
-validationUserDetails $firstName $namePattern
-validationUserDetails $lastname $namePattern
-validationUserDetails $emailAddress $emailPattern
-#function of validation for mobile number
-function validationMobileNumber(){
-	if [[ $numberM =~ $numberPatteren ]]
+validationFirstName $firstName $namePattern
+
+#function of validation for last name
+function validationLastName(){
+	if [[ $lastName =~ $namePattern ]]
 	then
-		echo "valid"
+		echo "valid last name"
+	else
+		echo "Invalid last name"
+	fi
+}
+validationLastName $lastname $namePattern
+
+#function of validation for email address
+function validationEmailAddress(){
+	if [[ $emailAddress =~ $emailPattern ]]
+	then
+		echo "valid email Address"
 	else
 		echo "invalid"
+	fi
+}
+validationEmailAddress $emailAddress $emailPattern
+
+#function of validation for password
+function validationPassword(){
+	if [[ $passwordChar =~ $passwordPattern ]]
+	then
+		echo "valid password"
+	else
+		echo "Invalid password"
+	fi
+}
+validationPassword $passwordChar $passwordPattern
+
+#function of validation for mobile number
+#param mobile number
+# param regex pattern for mobile number
+function validationMobileNumber(){
+	if [[ $numberM =~ $numberPattern ]]
+	then
+		echo "valid mobile number"
+	else
+		echo "invalid mobile number"
 	fi
 }
 validationMobileNumber $numberM $numberPattern

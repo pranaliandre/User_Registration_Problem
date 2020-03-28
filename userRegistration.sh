fdd3122c0ce88dd4cd41b5c3fe69c2b929955e77
pranaliandre <pranaliandre06@gmail.com>
2020-03-28 07:08:42 -0500

    Added: validation of mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,12 +6,16 @@ echo "Welcome to User Registration"
 namePattern="([A-Z]{1}[a-z]{2}$)"
 #regex pattern for email address
 emailPattern="abc((\.[A-Z]+[a-z]*[0-9]*)|(\.[A-Z]*[a-z]+[0-9]*)|(\.[A-Z]*[a-z]*[0-9]+)|^)?@bl\.co(\.[a-z]+){1,}"
+#regex pattern for mobile number
+numberPattern="^([0-9]{2}[ ]{1}[0-9]{10})$"
 #it take input from user first name
 read -p "Enter the first name: " firstName
 #it take input from userlast name
 read -p "Enter the last name: " lastName
 #it take input from user email address 
 read -p "Enter the email address: " emailAddress 
+#it take input from user mobile number
+read -p "Enter the mobile number: " numberM
 #function of validation for user Details
 function validationUserDetails(){
 	local userName=$1
@@ -26,3 +30,13 @@ function validationUserDetails(){
 validationUserDetails $firstName $namePattern
 validationUserDetails $lastname $namePattern
 validationUserDetails $emailAddress $emailPattern
+#function of validation for mobile number
+function validationMobileNumber(){
+	if [[ $numberM =~ $numberPatteren ]]
+	then
+		echo "valid"
+	else
+		echo "invalid"
+	fi
+}
+validationMobileNumber $numberM $numberPattern

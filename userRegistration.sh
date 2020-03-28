e07c56d6194af1b09cd640b5364eb06624bb5dca
pranaliandre <pranaliandre06@gmail.com>
2020-03-28 06:31:31 -0500

    Added: validation of last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,8 +4,9 @@ echo "Welcome to User Registration"
 #Regex patteren for first name 
 namePattern="([A-Z]{1}[a-z]{2}$)"
 #it take input from user first name
-read -p "Enter the first name " firstName
-
+read -p "Enter the first name: " firstName
+#it take input from userlast name
+read -p "Enter the last name: " lastName 
 #function of validation for user Details
 function validationUserDetails(){
 	local userName=$1
@@ -18,3 +19,4 @@ function validationUserDetails(){
 	fi
 } 
 validationUserDetails $firstName $namePattern
+validationUserDetails $lastname $namePattern
